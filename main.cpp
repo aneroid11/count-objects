58f7c7f9b4c96b6d2a25a3cff915f7d8db13a8ea
aneroid11 <a-e-v2003@yandex.by>
2023-06-09 12:49:48 +0300

    show black imagre

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <string>
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 int main()
 {
-    std::cout << "hello world!\n";
-
+    cv::Mat blackImage(cv::Mat::zeros(512, 512, 0));
+    cv::imshow("black", blackImage);
+    cv::waitKey();
     return 0;
 }
