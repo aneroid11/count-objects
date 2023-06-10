937e2bb5dffdff385e4cf2a3e883cca029d0e972
aneroid11 <a-e-v2003@yandex.by>
2023-06-10 23:00:47 +0300

    binary image

--- main.cpp
+++ main.cpp
@@ -4,18 +4,44 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+//image = cv2.imread('1.jpg')
+//gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+//blur = cv2.GaussianBlur(gray, (5,5), 0)
+//thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+//
+//cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+//cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+//for c in cnts:
+//area = cv2.contourArea(c)
+//if area > 10000:
+//cv2.drawContours(image, [c], -1, (36,255,12), 3)
+//
+//cv2.imwrite('thresh.png', thresh)
+//cv2.imwrite('image.png', image)
+//cv2.waitKey()
+
 int main()
 {
-    cv::Mat img = cv::imread("/home/lucky/dev/6sem/practice/count-objects/test.jpg");
-    if (img.empty())
-    {
-        std::cerr << "failed to read image\n";
-        exit(EXIT_FAILURE);
-    }
+    cv::Mat img = cv::imread("/home/lucky/dev/6sem/practice/count-objects/test2.jpg");
+    cv::Mat grayImg;
+    cv::cvtColor(img, grayImg, cv::COLOR_RGB2GRAY);
 
-    cv::circle(img, {50, 50}, 30, cv::Scalar(200, 100, 200), 5);
+    cv::Mat blurredImg;
+    cv::GaussianBlur(grayImg, blurredImg, cv::Size(15, 15), 0);
 
-    cv::imshow("img", img);
+    cv::Mat binaryImg;
+    cv::threshold(blurredImg, binaryImg, 0, 255,
+                  cv::THRESH_BINARY_INV + cv::THRESH_OTSU);
+
+    cv::imshow("window", binaryImg);
+//    cv::circle(img, {50, 50}, 30, cv::Scalar(200, 100, 200), 5);
+//    cv::Mat templateImg = cv::imread("/home/lucky/dev/6sem/practice/count-objects/template.jpg");
+
+//    cv::Mat result;
+//    cv::matchTemplate(img, templateImg, result, cv::TM_CCOEFF_NORMED);
+
+//    cv::imshow("matchTemplate", result);
+
     cv::waitKey();
     return 0;
 }
