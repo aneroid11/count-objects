6e040bd9c975964ce83aff5429e9255495e7fbbf
aneroid11 <a-e-v2003@yandex.by>
2023-06-15 14:24:31 +0300

    correct objects sizes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+const cv::Vec4b BG_COLOR = cv::Vec4b(0, 0, 0, 0);
+
 void showImg(const cv::Mat& img)
 {
     cv::imshow("", img);
@@ -55,7 +57,7 @@ void extractObjects(const cv::Mat& img, const std::vector<std::vector<cv::Point>
                 }
                 else
                 {
-                    newObj.at<cv::Vec4b>(y, x) = cv::Vec4b(0, 0, 0, 0);
+                    newObj.at<cv::Vec4b>(y, x) = BG_COLOR;
                 }
             }
         }
@@ -92,7 +94,6 @@ void rotateImg(const cv::Mat& srcImg, cv::Mat& dstImg, const double angle)
 
     std::cout << angle << "\n";
     cv::Mat rotM = cv::getRotationMatrix2D(center, angle, 1.0);
-//    cv::warpAffine(srcImg, dstImg, rotM, cv::Size(width * 2, height * 2));
 
     const cv::Rect bbox = cv::RotatedRect(cv::Point(),
                                           srcImg.size(),
@@ -115,8 +116,39 @@ void rotateObjects(std::vector<cv::Mat>& objects, const std::vector<std::vector<
 
 void compareObjects(const std::vector<cv::Mat>& objects, const int o1, const int o2)
 {
+    cv::Mat obj1 = objects[o1];
+    cv::Mat obj2 = objects[o2];
+
+    const int w1 = obj1.cols;
+    const int w2 = obj2.cols;
+    const int h1 = obj1.rows;
+    const int h2 = obj2.rows;
+
+    if (w1 < w2 && h1 > h2 || w1 > w2 && h1 < h2)
+    {
+        const int area1 = w1 * h1;
+        const int area2 = w2 * h2;
+        const int widthToAdd = abs(w1 - w2);
+
+        if (area1 < area2)
+        {
+            cv::copyMakeBorder(obj1, obj1,
+                               0, 0, widthToAdd / 2, widthToAdd / 2,
+                               cv::BORDER_CONSTANT,
+                               BG_COLOR);
+        }
+        else
+        {
+            cv::copyMakeBorder(obj2, obj2,
+                               0, 0, widthToAdd / 2, widthToAdd / 2,
+                               cv::BORDER_CONSTANT,
+                               BG_COLOR);
+        }
+    }
+
     cv::Mat result;
-    cv::matchTemplate(objects[o1], objects[o2], result, cv::TM_CCOEFF_NORMED);
+    cv::matchTemplate(obj1, obj2, result, cv::TM_CCOEFF_NORMED);
+    showImg(result);
 
     double maxVal;
     cv::minMaxLoc(result, nullptr, &maxVal);
@@ -137,8 +169,7 @@ int main()
 
     showObjects(objects);
 
-    // compare images 0 and 2
-    compareObjects(objects, 2, 4);
+    compareObjects(objects, 1, 3);
 
     return 0;
 }
